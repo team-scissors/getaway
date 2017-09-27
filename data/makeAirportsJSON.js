f095ce6badb832eeed476ebb7f514d3ec6a48dfe
Tim <theshuo@gmail.com>
2017-09-27 18:41:06 -0500

    Changes airports csv reader to US only

--- data/makeAirportsJSON.js
+++ data/makeAirportsJSON.js
@@ -2,14 +2,14 @@ const path = require('path');
 const csv = require('csvtojson');
 const jsonfile = require('jsonfile');
 
-var file = './airports.json';
+var file = './airports_USA.json';
 
 const csvFilePath = path.join(__dirname, './global_airports.csv');
 const allAirports = [];
 csv()
   .fromFile(csvFilePath)
   .on('json', (jsonObj) => {
-    allAirports.push(jsonObj);
+    if (jsonObj.country === 'United States') allAirports.push(jsonObj);
   })
   .on('done', (error) => {
     console.log('writing json');
