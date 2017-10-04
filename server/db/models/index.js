d3a6588eba197792d604c3e3a82c06f2e9b14c0e
Tim <theshuo@gmail.com>
2017-10-04 11:20:01 -0500

    Adds flightprice associations to seed file

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -9,15 +9,25 @@ Trip.belongsTo(User);
 
 // Each trip has one departing airport and many possible arrival airports.
 // The possible destinations are stored in the airports column as an array.
-Trip.belongsTo(Airport, {foreignKey: 'departFrom'});
+Trip.belongsTo(Airport, { foreignKey: 'departFrom' });
 
 // FlightPrice stores the price between two airports, from and to.
 // Note that the price from one airport to another varies depending on the
 // departure date (its more expensive to fly from CHI to LON tomorrow morning
 // than three months from now). Also, the flight price can change over time.
 // Refer to the created_at column to see historical data.
-FlightPrice.belongsTo(Airport, {as: 'from'});
-FlightPrice.belongsTo(Airport, {as: 'to'});
+Airport.belongsToMany(Airport, {
+  as: 'fromAirport',
+  through: 'FlightPrice',
+  foreignKey: 'toAirportId',
+});
+Airport.belongsToMany(Airport, {
+  as: 'toAirport',
+  through: 'FlightPrice',
+  foreignKey: 'fromAirportId',
+});
+// FlightPrice.belongsTo(Airport, { as: 'from' });
+// FlightPrice.belongsTo(Airport, { as: 'to' });
 
 module.exports = {
   User,
--- server/seed.js
+++ server/seed.js
@@ -1,12 +1,8 @@
 const moment = require('moment');
 const airports = require('../data/nonDuplicate_airports.json');
 const db = require('./db');
-const {
-  User,
-  Airport,
-  Trip,
-  FlightPrice,
-} = require('./db/models');
+const { User, Airport, Trip, FlightPrice } = require('./db/models');
+const topAirports = require('../data/topAirports.json');
 const Promise = require('bluebird');
 
 //iata_faa is the abbrv
@@ -15,8 +11,9 @@ const Promise = require('bluebird');
 
 /* ---------- Set up airports data ---------- */
 
-const createAirports = ((airports) => (
-  Promise.all(airports.map(airport => (
+const createAirports = airports =>
+  Promise.all(
+    airports.map(airport =>
       Airport.create({
         name: airport.name,
         abbrv: airport.iata_faa,
@@ -24,9 +21,9 @@ const createAirports = ((airports) => (
         country: airport.country,
         longitude: airport.longitude,
         latitude: airport.latitude,
-      })
-  )))
-));
+      }),
+    ),
+  );
 
 /* ---------- Set up users ---------- */
 
@@ -51,34 +48,36 @@ const fakeUsers = [
   },
 ];
 
-const createUsers = (users => (
-  Promise.all(users.map(user => (
-    User.create(user)
-  )))
-));
+const createUsers = users => Promise.all(users.map(user => User.create(user)));
 
 /* ---------- Set up trips ---------- */
 
 const fakeTrips = [
   {
     name: 'aaah! i need to run from the law!',
-    airports: [ 1, 200, 75 ],
+    airports: [1, 200, 75],
     departFrom: 5275,
-    departAt: moment().add(1, 'days').format(),
+    departAt: moment()
+      .add(1, 'days')
+      .format(),
     userId: 1,
   },
   {
     name: 'looking for a nice getaway',
-    airports: [ 220 ],
+    airports: [220],
     departFrom: 45,
-    departAt: moment().add(21, 'days').format(),
+    departAt: moment()
+      .add(21, 'days')
+      .format(),
     userId: 2,
   },
   {
     name: 'where even is papau new guinea?!1?',
-    airports: [ 1, 2, 3 ],
+    airports: [1, 2, 3],
     departFrom: 51,
-    departAt: moment().add(3, 'months').format(),
+    departAt: moment()
+      .add(3, 'months')
+      .format(),
     userId: 3,
   },
 ];
@@ -88,16 +87,51 @@ const seed = () => {
   return Promise.all([
     createAirports(airports),
     createUsers(fakeUsers),
-  ])
-  .spread( (airports, users) => {
-    const createTrips = fakeTrips.map(trip => {
-      return Trip.create(trip);
+  ]).spread((airports, users) => {
+    const topCreatedAirports = airports.filter(airport => {
+      return (
+        topAirports.find(searchAirport => {
+          return searchAirport.iata_faa === airport.abbrv;
+        }) !== undefined
+      );
     });
-    return Promise.all(createTrips);
+    // For topCreatedAirports we need to make complete bipartite graph of prices
+
+    const createPrices = topCreatedAirports.map(fromAirport => {
+      return Promise.all(
+        topCreatedAirports.map(toAirport => {
+          return fromAirport.addToAirport(toAirport, {
+            through: {
+              price: 1,
+              departAt: Date.now(),
+            },
+          });
+        }),
+      );
+      // topCreatedAirports.map(toAirport => {
+      //   fromAirport.addTo(toAirport, { as: 'from', through: FlightPrice });
+      // });
+    });
+
+    // const createPrices = Promise.map(topCreatedAirports, fromAirport => {
+    //   return topCreatedAirports.map(toAirport => {
+    //     fromAirport.addTo(toAirport, { as: 'from', through: FlightPrice });
+    //   });
+    // });
+
+    // const createTrips = fakeTrips.map(trip => {
+    //   return Trip.create(trip);
+    // });
+
+    return Promise.all(createPrices);
+
+    // return Promise.all([createTrips, createPrices]);
+    // return Promise.all([createTrips, createPrices]);
   });
 };
 
-db.sync({ force: true})
+db
+  .sync({ force: true })
   .then(() => {
     console.log('Seeding database');
     return seed();
@@ -105,10 +139,10 @@ db.sync({ force: true})
   .then(() => {
     console.log('Seeding successful!');
   })
-  .catch((err) => {
+  .catch(err => {
     console.log('Error from seeding: ', err);
   })
   .then(() => {
     db.close();
     return null;
-});
+  });
