4c3286f68a1ba4e2ccbf0493e0bb8893560e5177
Joon Kim <joon3575@gmail.com>
2017-10-05 10:57:50 -0500

    Filtered result based on the top airports

--- server/api/flightPrices.js
+++ server/api/flightPrices.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const unirest = require('unirest');
-
+const topAirports = require('../../data/topAirports.json');
 
 /*Use Amadeus API to fetch flight ticket prices
 e.g) origin=BOS&departure_date=2015-09-06--2015-09-26&duration=7--9&max_price=500
@@ -24,22 +24,28 @@ expected Output:
 }
 */
 
-const getFlightPrices = (origin, departureDate, maxPrice) => {
+const getFlightPrices = (origin, departureDate, maxPrice, apiKey="pryQGXaCG5zkd5P3yAGEQbWX9uMawvpT") => {
   return new Promise((resolve, reject) => {
-    unirest.get(`http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=${origin}&departure_date=${departureDate}&max_price=${maxPrice}&apikey=pryQGXaCG5zkd5P3yAGEQbWX9uMawvpT`)
+    unirest.get(`http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=${origin}&departure_date=${departureDate}&max_price=${maxPrice}&apikey=${apiKey}`)
       .header("Accept", "application/json") 
       .end(result => {
-        if (result.status) resolve(result.body);
+        if (result.status === 200) resolve(result.body);
         else reject(`Failed to fetch flight prices from: ${origin}`);
       });
   });
 };
 
+//Fetches flights with destinations that are in the top 100 airport list
 getFlightPrices(origin="BOS", departureDate="2017-10-20", maxPrice="1000")
-  .then(data => console.log(data))
-  .catch(console.error)
-
-
-
-
-
+  .then(bigObject => {
+    return bigObject.results
+  })
+  .then(airports => airports.filter(airport => {
+    return topAirports.find( searchAirport => {
+      return searchAirport.iata_faa === airport.destination;
+    }) !== undefined;
+  }))
+  .then(topDestinations => {
+    console.log(topDestinations.length);
+  })
+  .catch(console.error);
