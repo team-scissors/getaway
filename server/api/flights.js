a8defe0eb5833fa90607fc451e5be45b5e23f23c
Ashish Malav <aashmalav@gmail.com>
2017-10-06 14:08:32 -0500

    api for airport and flights working

--- server/api/flights.js
+++ server/api/flights.js
@@ -3,7 +3,25 @@ const {Flight} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  Flight.findAll()
-    .then(flights => res.json(flights))
-    .catch(next);
+  if(req.query.departureDate && req.query.maxPrice ){
+    Flight.findAll({
+      where: {
+        departAt: req.query.departureDate,
+        price: {
+          lte: req.query.maxPrice
+        }
+      }
+    })
+      .then(flights => res.json(flights))
+      .catch(next);
+  }
+  else if(req.query.departureDate){
+    Flight.findAll({
+      where: {
+        departAt: req.query.departureDate,
+      }
+    })
+      .then(flights => res.json(flights))
+      .catch(next);
+  }
 })
