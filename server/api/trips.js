6782e3722505830f67e9cc1df410f5471e564dd5
Finn Terdal <fterdal@gmail.com>
2017-10-07 18:26:32 -0500

    GET request for trips by user id

--- server/api/trips.js
+++ server/api/trips.js
@@ -2,17 +2,26 @@
 const router = require('express').Router();
 const {
   Trip,
-  Flights,
+  Flight,
   User,
 } = require('../db/models');
 module.exports = router;
 
 // get trips by userId
 router.get('/:userId', (req, res, next) => {
-
+  Trip.findAll({
+    include: [Flight],
+    where: {
+      userId: req.params.userId
+    }
+  })
+  .then(trips => res.send(trips))
+  .catch(next);
 });
 
 // Create a new trip
 router.post('/', (req, res, next) => {
-
+  const body = req.body;
+  Trip.create(body)
+  .then()
 });
