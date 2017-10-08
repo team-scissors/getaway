d874ca26c3d3b5843ced8c045142b1e813bf9c13
Finn Terdal <fterdal@gmail.com>
2017-10-07 19:23:56 -0500

    PUT request to associate an existing trip with an existing flight

--- server/api/trips.js
+++ server/api/trips.js
@@ -1,5 +1,6 @@
-/* eslint new-cap:0 */
+/* eslint new-cap:0 no-shadow:0 */
 const router = require('express').Router();
+const Promise = require('bluebird');
 const {
   Trip,
   Flight,
@@ -19,9 +20,22 @@ router.get('/:userId', (req, res, next) => {
   .catch(next);
 });
 
+// Adds a flight to an existing trip
+router.put('/:tripId/:flightId', (req, res, next) => {
+  const trip = Trip.findById(req.params.tripId);
+  const flight = Flight.findById(req.params.flightId);
+  Promise.all([trip, flight])
+  .spread( (trip, flight) => {
+    trip.addFlight(flight);
+  })
+  .then(trip => res.send(trip))
+  .catch(next);
+});
+
 // Create a new trip
 router.post('/', (req, res, next) => {
   const body = req.body;
   Trip.create(body)
-  .then()
+  .then(newTrip => res.send(newTrip))
+  .catch(next);
 });
