2985ba806155abd8a4e6cce243455ffa75ca0cb5
Joon Kim <joon3575@gmail.com>
2017-10-09 11:29:11 -0500

    Change timeout

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,10 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/getaway', {
-    logging: false
+    logging: false,
+    dialetOptions: {
+      timeout:60,
+    }
   }
 )
 module.exports = db
