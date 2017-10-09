4a7e6008e0663fd45c1f0e7e7ef052645a237e66
Finn Terdal <fterdal@gmail.com>
2017-10-09 15:39:47 -0500

    Adds endpoint to filter flights by date

--- server/api/flights.js
+++ server/api/flights.js
@@ -1,10 +1,10 @@
-const router = require('express').Router()
-const {Flight} = require('../db/models')
-module.exports = router
+const router = require('express').Router();
+const {Flight} = require('../db/models');
+module.exports = router;
 
 router.get('/', (req, res, next) => {
   //find flights by departure date and max price
-  if(req.query.departureDate && req.query.maxPrice ){
+  if (req.query.departureDate && req.query.maxPrice ){
     Flight.findAll({
       where: {
         departAt: req.query.departureDate,
@@ -17,7 +17,7 @@ router.get('/', (req, res, next) => {
       .catch(next);
   }
   //find flights by departure date
-  else if(req.query.departureDate){
+  else if (req.query.departureDate){
     Flight.findAll({
       where: {
         departAt: req.query.departureDate,
@@ -26,4 +26,20 @@ router.get('/', (req, res, next) => {
       .then(flights => res.json(flights))
       .catch(next);
   }
-})
+});
+
+// Finds all flights by departure date. Make sure the date param
+// looks something like '02-01-2018', NOT '02/01/2018' OR '2-1-18'
+router.get('/:date', async (req, res, next) => {
+  const date = req.params.date;
+  try {
+    const filteredFlights = await Flight.findAll({
+      where: {
+        departAt: date,
+      }
+    })
+    res.json(filteredFlights);
+  } catch(err) {
+    next(err)
+  }
+});
