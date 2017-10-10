f57c25adef40d9c0833aaf672cae7dd7518b7141
Joon Kim <joon3575@gmail.com>
2017-10-10 15:57:56 -0500

    Redirect complete

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -12,10 +12,13 @@ class Checkout extends Component {
       key: 'pk_test_9my2xxrckuwRluIePEZcdRbt',
       image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
       locale: 'auto',
-      token: function(token) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-      }
+      token: (token) => this.props.history.push('/')
+      
+      // function(token) { //history 
+      //   this.props.history.push("/")
+      //   // You can access the token ID with `token.id`.
+      //   // Get the token ID to your server-side code for use.
+      // }
     });
   }
 
@@ -33,49 +36,51 @@ class Checkout extends Component {
       <section className="section">
           <h2 className="has-text-centered">Trip Name {trip && `: ${tripName}`}</h2>
         {trip.length > 0 ? (
-          <div className="card trip-list">
-            <div className="panel">
-              {
-                trip.map((flight, idx) => {
-                return (
-                  <div className="panel-block" key={idx}>
-                    <div className="level flight-list-item">
-                      <div className="level-item has-text-centered">
-                        <div>
-                          <p className="heading">From</p>
-                          <p className="title is-6">{flight.origin.abbrv}</p>
+          <div>
+            <div className="card trip-list">
+              <div className="panel">
+                {
+                  trip.map((flight, idx) => {
+                  return (
+                    <div className="panel-block" key={idx}>
+                      <div className="level flight-list-item">
+                        <div className="level-item has-text-centered">
+                          <div>
+                            <p className="heading">From</p>
+                            <p className="title is-6">{flight.origin.abbrv}</p>
+                          </div>
                         </div>
-                      </div>
-                      <div className="level-item has-text-centered">
-                        <div>
-                          <p className="heading">To</p>
-                          <p className="title is-6">{flight.dest.abbrv}</p>
+                        <div className="level-item has-text-centered">
+                          <div>
+                            <p className="heading">To</p>
+                            <p className="title is-6">{flight.dest.abbrv}</p>
+                          </div>
                         </div>
-                      </div>
-                      <div className="level-item has-text-centered">
-                        <div>
-                          <p className="heading">Date</p>
-                          <p className="title is-6">{flight.departAt}</p>
+                        <div className="level-item has-text-centered">
+                          <div>
+                            <p className="heading">Date</p>
+                            <p className="title is-6">{flight.departAt}</p>
+                          </div>
                         </div>
-                      </div>
-                      <div className="level-item has-text-centered">
-                        <div>
-                          <p className="heading">Price</p>
-                          <p className="title is-6">
-                            ${Math.trunc(flight.price)}
-                          </p>
+                        <div className="level-item has-text-centered">
+                          <div>
+                            <p className="heading">Price</p>
+                            <p className="title is-6">
+                              ${Math.trunc(flight.price)}
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })
-              }
+                  );
+                })
+                }
+              </div>
             </div>
-            <button className="button is-primary" onClick={()=> 
-              this.StripeHandler.open({
-                name: 'getAway!',description: tripName, amount: Math.trunc(totalPrice)*100
-            }) }>Purchase</button>
+            <button className="button is-primary is-pulled-right purchase" onClick={()=> 
+            this.StripeHandler.open({
+              name: 'getAway!',description: tripName, amount: Math.trunc(totalPrice)*100
+          }) }>Purchase</button>
           </div>
         ) : (
             ''
