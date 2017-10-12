95898274c88d8d26fb8c8b69666bd8c4dd5db74d
Tim <theshuo@gmail.com>
2017-10-12 11:58:50 -0500

    Building flight endpoint

--- server/api/geo.js
+++ server/api/geo.js
@@ -1,6 +1,26 @@
 const router = require('express').Router();
 const { Airport, Flight } = require('../db/models');
+// const turf = require('turf');
 module.exports = router;
+
+const constructTIN = (flights, origin) => {
+  const tinLayer = {
+    type: 'FeatureCollection',
+    features: [],
+  };
+
+  flights.forEach(flight => {
+    tinLayer.features.push({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [origin.latitude, origin.longitude],
+      },
+    });
+  });
+
+  return tinLayer;
+};
 
 router.get('/tin', (req, res, next) => {
   if (req.query.departureDate && req.query.maxPrice) {
@@ -9,19 +29,36 @@ router.get('/tin', (req, res, next) => {
       where: {
         abbrv: origin,
       },
-    }).then(originAirport => {
-      console.log('airport:', originAirport);
-      Flight.findAll({
-        where: {
-          departAt: req.query.departureDate,
-          fromId: originAirport.id,
-          price: {
-            lte: req.query.maxPrice,
-          },
+      include: [
+        {
+          model: Airport,
+          as: 'toAirport',
         },
-      })
-        .then(flights => res.json(flights))
-        .catch(next);
+      ],
+    }).then(originAirport => {
+      res.json(originAirport);
     });
+    // Airport.findOne({
+    //   where: {
+    //     abbrv: origin,
+    //   },
+    // }).then(originAirport => {
+    //   console.log('airport:', originAirport);
+    //   Flight.findAll({
+    //     where: {
+    //       departAt: req.query.departureDate,
+    //       fromId: originAirport.id,
+    //       price: {
+    //         lte: req.query.maxPrice,
+    //       },
+    //     },
+    //     include: [
+    //       { model: Airport, as: 'toId' },
+    //       { model: Airport, as: 'fromId' },
+    //     ],
+    //   })
+    //     .then(flights => res.json(flights))
+    //     .catch(next);
+    // });
   }
 });
